968fb53ed7fad1fafd78b914350b602b09f95923
Martin Almvik <martinalmvik@hotmail.com>
2016-03-18 13:34:03 +0100

    Seems as if leaflet.js works better. Added it

--- app/Controllers/spreadController.js
+++ app/Controllers/spreadController.js
@@ -60,6 +60,29 @@ angular.module('flutrack.controllers', ['flutrack.services', 'rzModule'])
                 ]
             }
         ];
+        map = new google.maps.Map(document.getElementById('map'), mapOptions);
+        var styledMapType = new google.maps.StyledMapType(style, {map: map, name: 'Styled Map'});
+        map.mapTypes.set('map-style', styledMapType);
+        map.setMapTypeId('map-style');
+        var heatmap = new HeatmapOverlay(map,
+            {
+                // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+                "radius": 2,
+                "maxOpacity": 1,
+                // scales the radius based on map zoom
+                "scaleRadius": true,
+                // if set to false the heatmap uses the global maximum for colorization
+                // if activated: uses the data maximum within the current map boundaries
+                //   (there will always be a red spot with useLocalExtremas true)
+                "useLocalExtrema": false,
+                // which field name in your data represents the latitude - default "lat"
+                latField: 'lat',
+                // which field name in your data represents the longitude - default "lng"
+                lngField: 'lng',
+                // which field name in your data represents the data value - default "value"
+                valueField: 'count'
+            }
+        );
         HttpService.get('/api/prediction', 'index=14&day=' + $scope.daySlider).then(function (res) {
             var data = [];
             for (var i = 0; i < res.data.length; i++) {
@@ -71,7 +94,6 @@ angular.module('flutrack.controllers', ['flutrack.services', 'rzModule'])
                     });
                 }
             }
-            map = new google.maps.Map(document.getElementById('map'), mapOptions);
             //var heatMap = new google.maps.visualization.HeatmapLayer({
             //    data: data,
             //    map: map,
@@ -80,37 +102,14 @@ angular.module('flutrack.controllers', ['flutrack.services', 'rzModule'])
             //});
             //
             //
-            var styledMapType = new google.maps.StyledMapType(style, {map: map, name: 'Styled Map'});
-            map.mapTypes.set('map-style', styledMapType);
-            map.setMapTypeId('map-style');
             //
-            //$scope.map = map;
-            var heatmap = new HeatmapOverlay(map,
-                {
-                    // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-                    "radius": 2,
-                    "maxOpacity": 1,
-                    // scales the radius based on map zoom
-                    "scaleRadius": true,
-                    // if set to false the heatmap uses the global maximum for colorization
-                    // if activated: uses the data maximum within the current map boundaries
-                    //   (there will always be a red spot with useLocalExtremas true)
-                    "useLocalExtrema": true,
-                    // which field name in your data represents the latitude - default "lat"
-                    latField: 'lat',
-                    // which field name in your data represents the longitude - default "lng"
-                    lngField: 'lng',
-                    // which field name in your data represents the data value - default "value"
-                    valueField: 'count'
-                }
-            );
+            //$scope.map = map
 
             var testData = {
-                max: 8,
+                max: 60000,
                 data: data
             };
             heatmap.setData(testData);
-            $scope.map = map;
 
         }, function (error) {
             console.log('Got an error from the API:' + error.message);
@@ -127,7 +126,6 @@ angular.module('flutrack.controllers', ['flutrack.services', 'rzModule'])
                         });
                     }
                 }
-                map = new google.maps.Map(document.getElementById('map'), mapOptions);
                 //var heatMap = new google.maps.visualization.HeatmapLayer({
                 //    data: data,
                 //    map: map,
@@ -136,37 +134,14 @@ angular.module('flutrack.controllers', ['flutrack.services', 'rzModule'])
                 //});
                 //
                 //
-                var styledMapType = new google.maps.StyledMapType(style, {map: map, name: 'Styled Map'});
-                map.mapTypes.set('map-style', styledMapType);
-                map.setMapTypeId('map-style');
                 //
                 //$scope.map = map;
-                var heatmap = new HeatmapOverlay(map,
-                    {
-                        // radius should be small ONLY if scaleRadius is true (or small radius is intended)
-                        "radius": 2,
-                        "maxOpacity": 1,
-                        // scales the radius based on map zoom
-                        "scaleRadius": true,
-                        // if set to false the heatmap uses the global maximum for colorization
-                        // if activated: uses the data maximum within the current map boundaries
-                        //   (there will always be a red spot with useLocalExtremas true)
-                        "useLocalExtrema": false,
-                        // which field name in your data represents the latitude - default "lat"
-                        latField: 'lat',
-                        // which field name in your data represents the longitude - default "lng"
-                        lngField: 'lng',
-                        // which field name in your data represents the data value - default "value"
-                        valueField: 'count'
-                    }
-                );
 
                 var testData = {
                     max: 100000,
                     data: data
                 };
                 heatmap.setData(testData);
-                $scope.map = map;
             }, function (err) {
                 console.log('Got an error from the API:' + err.message);
             });
